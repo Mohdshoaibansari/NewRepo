a00e957ca473fde6afbcf04bee5e01cf3293af0b
Mohdshoaibansari <43471848+Mohdshoaibansari@users.noreply.github.com>
2020-08-29 16:03:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,7 +16,16 @@ if __name__ == "__main__":
             else:
                 #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
                 name=row[0]
-                groups=row[1].split(',')
-                policies=row[2].split(',')
+                try:
+                    groups=row[1].split(',')
+                    policies=row[2].split(',')
+                except IndexError:
+                    groups=null
+                try:
+                    policies=row[2].split(',')
+                except IndexError:
+                    policies=null
+                
+                    
                 print(name,groups,policies)
                 line_count += 1
