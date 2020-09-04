a4eaefacbdf7f1ddc1fc6d79428ace9cd81d1908
mohd-shoaib <mohd.shoaib@allianz.com>
2020-09-04 13:02:36 +0530

    Class Code

---
+++ run.py
@@ -0,0 +1,33 @@
+import test
+import csv
+
+
+
+if __name__ == "__main__":
+
+    run= test.Usercreate()
+    name=""
+    groups=[]
+    policies=[]
+    with open('/Users/mohdshoaib/SharedServices/usercreate/Users.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='$')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+
+                name=row[0]
+                try:
+                    groups=row[1].split(',')
+                    run.create_user(name)
+                    run.group_addition(name,groups)
+                except IndexError:
+                    groups=[]
+                try:
+                    policies=row[2].split(',')
+                    run.create_user(name)
+                    run.policy_attach(name, policies)
+                except IndexError:
+                    policies=[]
